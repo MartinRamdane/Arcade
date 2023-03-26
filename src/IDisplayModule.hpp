17bdbcb0df746cee064d81e2bba04a67f47e4fd1
Ramdane Martin <martin.ramdane@epitech.eu>
2023-03-26 11:17:23 +0200

    add commentary/documentatio for Intefaces

--- src/IDisplayModule.hpp
+++ src/IDisplayModule.hpp
@@ -14,11 +14,39 @@
 class IDisplayModule {
     public:
         virtual ~IDisplayModule () = default ;
+
+        /**
+        ** @brief Must be called before any other method to initialize the display
+        ** @param entites The entities to display
+        **/
         virtual void init(std::map<std::string, IGameModule::Entity> &entites) = 0;
+
+        /**
+        ** @brief Must be called to stop the display and free all resources
+        **/
         virtual void stop() = 0;
+
+        /**
+        ** @brief Must be called to update the display, this method must be called before draw
+        ** @param entites The entities to display
+        **/
         virtual void update(std::map<std::string, IGameModule::Entity> &entities) = 0;
+
+        /**
+        ** @brief Must be called to draw the display, this method must be called after update
+        **/
         virtual void draw() = 0;
+
+        /**
+        ** @brief Must be called to get the event from the display
+        ** @return The event as a string
+        **/
         virtual std::string getEvent() = 0;
+
+        /**
+        ** @brief Must be called to get the name of the library used
+        ** @return The name of the library
+        **/
         virtual const std::string &getName() const = 0;
 };
 
--- src/IGameModule.hpp
+++ src/IGameModule.hpp
@@ -46,11 +46,51 @@ class IGameModule {
         };
 
         virtual ~IGameModule () = default ;
+
+        /**
+        ** @brief Start the game, initialize the game and all the variables
+        ** @param username The username of the player
+        **/
         virtual void startGame(std::string username) = 0;
+
+        /**
+        ** @brief Must be called to know if the game is over
+        ** @return true if the game is over, false otherwise
+        **/
         virtual bool isGameOver() = 0;
+
+        /**
+        ** @brief Update the game, must be called every frame
+        ** @param key The key pressed by the player
+        **/
         virtual void update(std::string key) = 0;
+
+        /**
+        ** @brief Get the infos of the game, the entities to display
+        ** @return A map of the entities to display
+        **/
         virtual std::map<std::string, IGameModule::Entity> &getInfos() = 0;
+
+        /**
+        ** @brief Create an entity, an entity is an object to display
+        ** @param file The path to the file of the entity if it's a sprite
+        ** @param text The text to display if it's a text
+        ** @param color The color of the text if it's a text
+        ** @param background_color The background color of the text if it's a text
+        ** @param x The x position of the entity
+        ** @param y The y position of the entity
+        ** @param type The type of the entity)
+        ** @param xSprite The x position of the sprite if it's a sprite
+        ** @param ySprite The y position of the sprite if it's a sprite
+        ** @param fontSize The size of the font if it's a text
+        ** @return The entity created
+        **/
         virtual IGameModule::Entity createEntity(std::string file, std::string text, std::string color, std::string background_color, float x, float y, ENTITY_TYPE type, float xSprite = 0, float ySprite = 0, int fontSize = 0) = 0;
+
+        /**
+        ** @brief Get the status of the game, the status can be MENU, PAUSED, IN_GAME or FINISHED
+        ** @return The status of the game
+        **/
         virtual IGameModule::GAME_STATUS getGameStatus() = 0;
 
     protected:
